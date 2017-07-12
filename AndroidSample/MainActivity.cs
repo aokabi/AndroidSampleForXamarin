2b29304d66b7544ff1fd01c009ee7ba75f8c0bd5
aokabi <aokabit@gmail.com>
2017-07-12 16:04:19 +0900

    プロジェクト ファイルを追加します。

---
+++ AndroidSample/MainActivity.cs
@@ -0,0 +1,24 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace AndroidSample
+{
+    [Activity(Label = "AndroidSample", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        int count = 0;
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            this.SetContentView (Resource.Layout.Main);
+
+            var button = this.FindViewById<Button>(Resource.Id.SampleButton);
+
+            button.Click += delegate { button.Text = string.Format("{0} clicks!", count++); };
+        }
+    }
+}
+
